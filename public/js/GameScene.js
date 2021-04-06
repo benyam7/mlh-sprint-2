991e8cd6ba53a12fb762a8e7a61bd64adac016f6
Hakeem Beldjoudi <ih_beldjoudi@esi.dz>
2021-04-06 13:27:48 +0100

    tmp: add ground

--- public/js/GameScene.js
+++ public/js/GameScene.js
@@ -1,7 +1,5 @@
-class GameScene extends Phaser.Scene
-{
-    constructor()
-    {
+class GameScene extends Phaser.Scene {
+    constructor() {
         super({
             key: "GameScene",
         });
@@ -10,24 +8,45 @@ class GameScene extends Phaser.Scene
         this.tilesets = 0
     }
 
-    init(data) 
-    {
+    init(data) {
         this.tilesets = data.tilesets;
     }
 
-    preload()
-    {
-        
+    preload() {
+
     }
 
-    create()
-    {
+    create() {
         const map = this.make.tilemap({ key: "env" });
         const groundLayer = map.addTilesetImage("bg1");
         let tileLayer = map
-            .createLayer("TileLayer1", [groundLayer, ], 0, 0)
+            .createLayer("TileLayer1", [groundLayer,], 0, 0)
             .setScale(0.5);
 
+        // create objects layer
+
+        this.objLayer = map.getObjectLayer("ObjectLayer1")["objects"];
+        this.objsGroup = this.physics.add.group();
+
+        this.objLayer.forEach((object) => {
+            let name = this.getObjPropertyFromGid(object.gid, "name");
+            let obj = this.objsGroup.create(object.x, object.y, name);
+            obj.name = name;
+            obj.setScale(0.79);
+            obj.enableBody = true;
+            obj.body.immovable = true;
+            obj.setX(Math.round(object.x * 0.5));
+            obj.setY(Math.round(object.y * 0.5));
+
+        });
+
+    }
+
+    getObjPropertyFromGid(gid, prop) {
+        for (let i = 0; i < this.tilesets.length; i++) {
+            let obj = this.tilesets[i];
+            if (obj.gid == gid) return obj[prop];
+        }
     }
 
 }
