695052875e3ab7eb5dc131a0af0556701d9ebd8f
Hakeem Beldjoudi <ih_beldjoudi@esi.dz>
2021-04-09 17:49:21 +0100

    add Panels and Visit Website Buttons over

--- public/js/GameScene.js
+++ public/js/GameScene.js
@@ -1,3 +1,9 @@
+
+const GamePointers = {
+  panelGids: [1563, 1564, 1565],
+  adBoxRefs: {1563: 'laptop', 1564: 'shoes', 1565: 'watch'}
+}
+
 class GameScene extends Phaser.Scene {
   constructor() {
     super({
@@ -22,6 +28,39 @@ class GameScene extends Phaser.Scene {
     this.playerMovement.setUpMovmentControls();
     this.cameras.main.startFollow(this.player);
     // this.cameras.main.roundPixels = true;
+
+    const map = this.make.tilemap({ key: "env" });
+
+    // create objects layer
+
+    this.objLayer = map.getObjectLayer("ObjectLayer1")["objects"];
+    this.objsGroup = this.physics.add.staticGroup();
+
+    this.objLayer.forEach((object) => {
+
+        let posScale = 0.498
+        let name = this.getObjPropertyFromGid(object.gid, "name");
+        let obj = this.objsGroup.create(object.x, object.y, name);
+        obj.name = name;
+        obj.setScale(0.7);
+        obj.enableBody = true;
+        obj.body.immovable = true;
+        obj.setX(Math.round(object.x * posScale));
+        obj.setY(Math.round(object.y * posScale));
+
+
+        if(GamePointers.panelGids.includes(object.gid)) // add panels here
+        {
+            this.createVisitButtons(obj.body.x * posScale, obj.body.y * posScale, object.gid)
+        }
+
+        if(name.includes('panel'))
+        {
+          obj.setScale(0.6)
+        }
+
+    });
+
   }
 
   getObjPropertyFromGid(gid, prop) {
@@ -77,15 +116,65 @@ class GameScene extends Phaser.Scene {
       repeat: -1,
       yoyo: true,
     });
+
+    // create misc buttons
   }
 
   update() {
     if (this.playerMovement.controls.direction === "left") {
-      this.player.x -= 2;
+      this.player.x -= 4;
       this.player.play("walk_left");
     } else if (this.playerMovement.controls.direction === "right") {
-      this.player.x += 2;
+      this.player.x += 4;
       this.player.play("walk_right");
     }
+  }
+
+  createVisitButtons(x, y, gid)
+  {
+    let progressBox = this.add.graphics();
+
+    let pbSettings = {
+      x: x,
+      y: y - 40,
+      width: 250,
+      height: 30,
+    };
+
+    progressBox.fillStyle(0x1d1d1d, 1);
+
+
+    const fillPBox = () => {
+      progressBox.fillRect(
+        pbSettings["x"],
+        pbSettings["y"],
+        pbSettings["width"],
+        pbSettings["height"]
+      );
+    }
+
+    fillPBox()
+
+    let btnX = pbSettings.x + (pbSettings.width / 2)
+    let btnY = pbSettings.y + (pbSettings.height / 2)
+
+    const visitButton = this.add.text(btnX, btnY, 'Visit Website', { fill: '#ffffff', fontSize: '15px', fontWeight: 'bold', fontFamily: 'curisve' });
+    visitButton.setOrigin(0.5, 0.5)
+    visitButton.setInteractive({ cursor: 'pointer' });
+
+    visitButton.on('pointerover', ev => {
+      progressBox.fillStyle(0x2d2d2d, 1);
+      fillPBox()
+    })
+
+    visitButton.on('pointerout', ev => {
+      progressBox.fillStyle(0x1d1d1d, 1);
+      fillPBox()
+    })
+
+    visitButton.on('pointerup', ev => {
+      // showBox of website
+      showBox(GamePointers.adBoxRefs[gid])
+    })
   }
 }
